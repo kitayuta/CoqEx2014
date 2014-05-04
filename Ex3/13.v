8d69b4d1348d03a48c5dd0e79d0574e46a40fce3
kitayuta <kitayuta@gmail.com>
2014-05-04 15:43:20 +0900

    Solve Task 13

---
+++ Ex3/13.v
@@ -0,0 +1,23 @@
+Inductive pos : Set :=
+| SO : pos
+| S : pos -> pos.
+
+Fixpoint plus(n m:pos) : pos :=
+  match n with
+    | SO => S m
+    | S p => S (p + m)
+  end
+    where "n + m" := (plus n m).
+
+Infix "+" := plus.
+
+Theorem plus_assoc : forall n m p, n + (m + p) = (n + m) + p.
+Proof.
+  intros.
+  induction n.
+  simpl.
+  reflexivity.
+  simpl.
+  f_equal.
+  assumption.
+Qed.
