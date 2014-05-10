947c7bac7303db3469912e78cb96c5e9b4db9be0
kitayuta <kitayuta@gmail.com>
2014-05-10 19:17:41 +0900

    Add some examples of Church encoding

---
+++ Ex4/ce_examples.v
@@ -0,0 +1,70 @@
+Definition True' : Prop := forall P : Prop, P -> P.
+Goal True <-> True'.
+Proof.
+  unfold iff, True'.
+  split.
+  intros.
+  assumption.
+  intro.
+  apply I.
+Qed.
+Goal True'.
+Proof.
+  unfold True'.
+  intros.
+  assumption.
+Qed.
+
+
+Definition False' : Prop := forall P : Prop, P.
+Goal False' <-> False.
+Proof.
+  unfold iff.
+  split.
+  intro.
+  apply H.
+  apply False_ind.
+Qed.
+Goal forall P : Prop, False' -> P.
+Proof.
+  intros.
+  apply H.
+Qed.
+
+Definition and' : Prop -> Prop -> Prop :=
+  fun A B => forall P : Prop, (A -> B -> P) -> P.
+Goal forall P Q : Prop, P /\ Q <-> and' P Q.
+Proof.
+  intros.
+  unfold iff, and'.
+  split.
+  intro.
+  destruct H.
+  intros.
+  exact (H1 H H0).
+  intro.
+  split.
+  apply H.
+  intros.
+  assumption.
+  apply H.
+  intros.
+  assumption.
+Qed.
+
+Definition or' : Prop -> Prop -> Prop :=
+  fun A B => forall P : Prop, (A -> P) -> (B -> P) -> P.
+Goal forall P Q : Prop, P \/ Q <-> or' P Q.
+Proof.
+  intros.
+  unfold iff, or'.
+  split.
+  intros.
+  destruct H.
+  exact (H0 H).
+  exact (H1 H).
+  intro.
+  apply H.
+  apply or_introl.
+  apply or_intror.
+Qed.
